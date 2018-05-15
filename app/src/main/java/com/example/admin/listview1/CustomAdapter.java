3751b117ea5582e2b949176322048252da7601b3
jagrati <jagratikatariya1996@gmail.com>
2018-05-15 14:54:00 +0530

    second commit

--- app/src/main/java/com/example/admin/listview1/CustomAdapter.java
+++ app/src/main/java/com/example/admin/listview1/CustomAdapter.java
@@ -16,39 +16,34 @@ import java.util.Random;
 
 
 public class CustomAdapter extends ArrayAdapter<String> {
-    public CustomAdapter(@NonNull Context context, String [] name) {
-        super(context, R.layout.custom_main,name);
+    public CustomAdapter(@NonNull Context context, String[] name) {
+        super(context, R.layout.custom_main, name);
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater myinflator = LayoutInflater.from(getContext());
-        View customview = myinflator.inflate(R.layout.custom_main,parent,false);
+        View customview = myinflator.inflate(R.layout.custom_main, parent, false);
         String nameposition = getItem(position);
         ImageView img = customview.findViewById(R.id.imgcircle);
         TextView textname = customview.findViewById(R.id.textname);
         TextView textinitial = customview.findViewById(R.id.textinitial);
         textname.setText(nameposition);
         char s = nameposition.charAt(0);
-        Log.d("DEBUG",s+"");
-        textinitial.setText(s+"");
+        Log.d("DEBUG", s + "");
+        textinitial.setText(s + "");
         Random random = new Random();
 
         int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
         img.setColorFilter(color);
-        for(int i=0; i<=position;i++)
-        {
-            if(i%2==0)
-            {
+        for (int i = 0; i <= position; i++) {
+            if (i % 2 == 0) {
                 customview.setBackgroundColor(Color.LTGRAY);
-            }
-            else
-            {
+            } else {
                 customview.setBackgroundColor(Color.WHITE);
             }
         }
         return customview;
-
     }
 }
